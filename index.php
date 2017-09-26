8cd3eef194de16425c9213ef8624d6c8f8a75c48
Guoqiang Zhang <guoqiang.zhang66@titansoft.com.sg>
2017-09-26 15:54:52 +0800

    abc

--- index.php
+++ index.php
@@ -6,16 +6,32 @@
 		<img src="" id="video" />
 		<script>
 		var num = 0;
+		var counter = 0;
+		var preload = 100;
+		var images = [];
 		(function(){
+			for(var i = 0; i<100; i++){
+				addImages();
+			}
+			
 			setInterval(function(){
-				$("#video").prop("src", "OutputPics/Demo20170925b" + pad(num, 5) + ".jpg");
-				num++;
+				$("#video").prop("src", images[0].src);
+				images.shift();
+				addImages();
+				counter++;
 			}, 1000/25);
 			})();
 			
 			function pad(num, size) {
 				var s = "000000000" + num;
 				return s.substr(s.length-size);
+			}
+			
+			function addImages(){
+				var image = new Image();
+				image.src = "OutputPics/Demo20170925b" + pad(num, 5) + ".jpg";
+				images.push(image);
+				num ++;
 			}
 		</script>
 	</body>
